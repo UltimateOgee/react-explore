583c3dd69b09a10ebb2db97b9141e253c7f1e6bd
Shubhankar Singh <shubhankarsingh@wta-mbp915.lan>
2021-07-03 16:25:37 -0700

    just some random hooks stuff

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//CSS
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const data = [
+  {
+    id: 1,
+    name: "wheel",
+    desc: "you need this for the inside of the tire",
+    img: "http://pngimg.com/uploads/car_wheel/small/car_wheel_PNG23305.png",
+  },
+  {
+    id: 2,
+    name: "baby car seat",
+    desc: "you need this to transport babies",
+    img: "https://i5.walmartimages.com/asr/e43c2722-40b9-446e-ae42-d950cfbfbba5.260ed804a3b47600c0f0522cb72cca46.jpeg?odnHeight=200&odnWidth=200&odnBg=ffffff",
+  },
+];
+
+function Cart() {
+  return (
+    <>
+      <h3>This is my product shopping cart:</h3>
+      <section className="productlist">
+        <CartItems />
+      </section>
+    </>
+  );
+}
+
+const CartItems = () => {
+  return (
+    <section>
+      {data.map((item) => {
+        return <Product className="productlist" key={item.id} {...item} />;
+      })}
+    </section>
+  );
+};
+
+const Product = ({ name, desc, img, children }) => {
+  return (
+    <article className="product">
+      <img src={img} alt="random product" />
+      <h1>{name}</h1>
+      <h4>{desc}</h4>
+      {children}
+    </article>
+  );
+};
+
+ReactDOM.render(<Cart />, document.getElementById("root"));
